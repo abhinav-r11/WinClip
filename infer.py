b0f1738dfd0412ac866d28e6065a2c7ede273584
Abhinav Raghuvanshi <78714364+abhinavr11@users.noreply.github.com>
2024-09-09 01:34:43 +0530

    Update infer.py

--- infer.py
+++ infer.py
@@ -49,7 +49,7 @@ def single_image_inference(model, image_path: str, device: str, resolution: int 
 
     # Post-process output
     #output = [o.cpu().numpy() for o in output]
-    output = output[0].numpy()  # Convert to numpy
+    #output = output[0].numpy()  # Convert to numpy
     output = denormalization(output)  # Apply denormalization if needed
 
     return output
